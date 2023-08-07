36df2b30ba51e4f8ed6aa283590392600b11dedb
weswai <waiting_lam@hotmail.com>
2023-08-07 14:18:16 -0700

    Update config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/weight-tracker/",
-
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/weight-tracker/'
+  : '/',
   plugins: [vue()],
 })
